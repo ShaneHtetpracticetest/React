d4b4743c9928366defb798864d0ee6275706ba6e
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-09-22 11:59:36 +0700

    mui error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+// import
 import React from 'react'
-import Header from './header'
-import Body from './body'
-import Footer from './footer'
-import Card from './Card'
+// import ResponsiveAppBar from './Appbar'
+import FetchData from './FetchData'
 
+// function
 const App = () => {
   return(
     <div>
-      <Header />
-      <Body />
-      <Card title='t1' text='something' />
-      <Card titel='t2' text='anything' />
-      <Footer />
+      {/* <ResponsiveAppBar /> */}
+      <FetchData />
     </div>
   )
 }
 
+// export default App
 export default App
